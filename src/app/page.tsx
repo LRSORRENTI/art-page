08ec9c2068e55e8851e1468f7f5406d5fc0fd968
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-03-02 08:43:24 -0600

    Added title for gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,13 @@ export default function Home() {
   // const toggleBurger = () => {
   //   setIsActive(!isActive);
   // };
-
+  // text-3xl mb-3 font-thin hover:font-extralight hover:cursor-pointer" style={{transition: ".2s ease-in"}}>about</p>
   return (
     <>
+    <div className='flex'>
     <NavDrawer/>
+    <h1 className='flex justify-center mx-auto text-slate-500 mt-4  font-thin text-5xl'>Lumina Gallery</h1>
+    </div>
     <HeroContainer/>
     </>
   );
