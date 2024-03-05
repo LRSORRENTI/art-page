e4a1310dab8195eb6ce1338f5e2b9247f3081bd1
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-03-05 08:13:01 -0600

    connected about on main container to render the About.tsx

--- src/components/HeroContainer.tsx
+++ src/components/HeroContainer.tsx
@@ -6,7 +6,7 @@ export function HeroContainer({ setCurrentSelection }: any){
                 <p className="mx-auto text-3xl mb-3 font-thin hover:font-extralight hover:cursor-pointer" style={{transition: ".2s ease-in"}} onClick={() => setCurrentSelection('coming')}>coming soon</p>
             </div>
             <div className=" pt-52 pb-52  border-t-transparent border-r-transparent border-b-transparent hidden lg:block" style={{ position: 'absolute', top: '40%', left:'90%', transform: 'translateY(-50%)', transition: ".2s ease" }}>
-            <p className=" mx-auto text-3xl mb-7 font-thin hover:font-extralight hover:cursor-pointer" style={{transition: ".2s ease-in"}}>about</p>
+            <p className=" mx-auto text-3xl mb-7 font-thin hover:font-extralight hover:cursor-pointer" style={{transition: ".2s ease-in"}}onClick={() => setCurrentSelection('about')} >about</p>
             <p className="mx-auto text-3xl mb-7 font-thin hover:font-extralight hover:cursor-pointer" style={{transition: ".2s ease-in"}}>gallery</p>
             <p className="mx-auto text-3xl font-thin hover:font-extralight hover:cursor-pointer " style={{transition: ".2s ease-in"}}>contact</p>
             </div>
